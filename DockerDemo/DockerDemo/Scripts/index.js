950123598d41814c4ebc8041928a28861834fa9f
Vishal Sharma <vishal@Vishals-MacBook-Pro.local>
2020-04-04 16:28:11 -0400

    My Docker files

---
+++ DockerDemo/DockerDemo/Scripts/index.js
@@ -0,0 +1,12 @@
+﻿const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hi Vishal');
+}
+);
+
+app.listen(8080, () => {
+    console.log('Listening to port 8080');
+}
+);
---
+++ visits/index.js
@@ -0,0 +1,20 @@
+﻿const express = require("express");
+const redis = require("redis");
+
+const app = express();
+const client = redis.createClient({
+    host: "redis-server",
+    port: 6379
+});
+    
+app.get("/", (req, res) => {
+    //process.exit(0)
+    client.get("visits", (err, visits) => {
+        res.send("Number of visits 1" + visits);
+        client.set("visits", parseInt(visits) + 1);
+    });
+});
+
+app.listen(8081, () => {
+    console.log('Listening to port 8081');
+});
